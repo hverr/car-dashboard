a2f1db0b63e0a92ad7e6cde4b157d505fd220454
Henri Verroken <henriverroken@gmail.com>
2016-10-14 15:36:27 +0000

    Use pretty printing for JSON responses

--- src/Dashboard/Server/Routes/Json.hs
+++ src/Dashboard/Server/Routes/Json.hs
@@ -1,7 +1,8 @@
 -- | Helper functions to work with JSON.
 module Dashboard.Server.Routes.Json where
 
-import Data.Aeson (ToJSON, encode)
+import Data.Aeson (ToJSON)
+import Data.Aeson.Encode.Pretty (encodePretty)
 
 import Network.HTTP.Types (hContentType, ok200)
 import Network.Wai (Response, responseLBS)
@@ -10,5 +11,5 @@ import Dashboard.Server.Monad (ServerT)
 
 -- | Jsonify a 'Route' function.
 jsonify :: ToJSON b => (a -> ServerT IO b) -> (a -> ServerT IO Response)
-jsonify f x = responseLBS ok200 hs . encode <$> f x
+jsonify f x = responseLBS ok200 hs . encodePretty <$> f x
   where hs = [(hContentType, "application/json")]
