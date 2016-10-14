3f7f27dca8279c0ff422417c6aab5d1b4ad3f570
Henri Verroken <henriverroken@gmail.com>
2016-10-14 23:10:14 +0000

    Update live car data in web page

--- data/html/app.js
+++ data/html/app.js
@@ -8,7 +8,39 @@ $(function() {
 
   function queryEngineData() {
     $.get('/api/engine/status').done(function(data) {
-      console.log("got: " + JSON.stringify(data));
+      updateValue("speed", data.vehicleSpeed, function(v) {
+        $('#vehicle-speed-input').val(v).trigger('change');
+      });
+      updateValue("engine RPM", data.engineRPM, function(v) {
+        $('#engine-rpm-input').val(v).trigger('change');
+      });
+      updateValue("fuel rate", data.engineFuelRate, function(v) {
+        $('#fuel-rate-value2').text(v);
+
+        if(data.vehicleSpeed.value) {
+          var x = v * 100 / data.vehicleSpeed.value;
+          var w = (x / 15 > 1) ? 100 : x * 100 / 15;
+          $('#fuel-rate-bar').attr('style', 'width: ' + w + '%;');
+          $('#fuel-rate-value').text(x);
+        } else {
+          $('#fuel-rate-bar').attr('style', 'width: 0%;');
+          $('#fuel-rate-value').html('&infin;');
+        }
+      });
+      updateValue("coolant temperature", data.engineCoolantTemperature, function(v) {
+        var w = (v / 180 > 1) ? 100 : v * 100 / 180;
+        $('#coolant-temp-value').text(v);
+        $('#coolant-temp-bar').attr('style', 'width: ' + w + '%;');
+      });
+      updateValue("intake air temperature", data.intakeAirTemperature, function(v) {
+        $('#intake-air-temp-value').text(v);
+      });
+      updateValue("throttle position", data.throttlePosition, function(v) {
+        $('#throttle-position-value').text(v);
+        $('#throttle-position-bar').attr('style', 'width: ' + v + '%;');
+      });
+    }).always(function() {
+      setTimeout(queryEngineData, 250);
     });
   }
 
@@ -20,6 +52,14 @@ $(function() {
     }).always(function() {
       setTimeout(queryEngineErrors, 500);
     });
+  }
+
+  function updateValue(humanName, data, f) {
+    if (data.error !== null) {
+      showError("Could not get " + humanName + ": " + value.error);
+      return;
+    }
+    f(data.value);
   }
 
   function showError(err) {
