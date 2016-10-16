6e0635797a9b7e7163429e04d28bc9366b080f78
Henri Verroken <henriverroken@gmail.com>
2016-10-16 14:19:16 +0000

    Add README and a script to build the deb

---
+++ README.md
@@ -0,0 +1,30 @@
+Car Dashboard
+=============
+
+Use your Android phone as a car dashboard.
+
+Developing
+----------
+
+```sh
+sudo apt-get install -y npm
+npm install
+stack build
+stack exec car-dashboard -- --simulator
+```
+
+Now point your browser to `http://127.0.0.1:8080`.
+
+
+Deployment
+----------
+
+The following steps are needed to deploy the application to a Raspberry Pi.
+
+```sh
+./package.sh
+dpkg -i dist/car-dashboard-*.deb
+systemctl daemon-reload
+systemctl enable car-dashboard.service
+systemctl start car-dashboard.service
+```
---
+++ package.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -ex
+
+npm install
+cabal install
+
+VERSION=$(awk '$1 == "version:" {print $2}' car-dashboard.cabal)
+REVISION=1
+
+DEB_ROOT="dist/car-dashboard_${VERSION}-${REVISION}"
+mkdir -p "$DEB_ROOT/DEBIAN/"
+mkdir -p "$DEB_ROOT/lib/systemd/system/"
+mkdir -p "$DEB_ROOT/usr/bin/"
+mkdir -p "$DEB_ROOT/usr/share/car-dashboard/"
+
+cp .cabal-sandbox/bin/car-dashboard "$DEB_ROOT/usr/bin/"
+cp car-dashboard.service "$DEB_ROOT/lib/systemd/system/"
+rsync -r data "$DEB_ROOT/usr/share/car-dashboard/"
+rsync -r node_modules "$DEB_ROOT/usr/share/car-dashboard/"
+
+cat <<EOF > "$DEB_ROOT/DEBIAN/control"
+Package: car-dashboard
+Version: ${VERSION}-${REVISION}
+Section: base
+Priority: optional
+Architecture: $(dpkg --print-architecture)
+Depends: zlib1g-dev, libncurses5-dev
+Maintainer: Henri Verroken <henriverroken@gmail.com>
+Description: Car Dashboard
+EOF
+
+cd dist
+dpkg-deb --build "car-dashboard_${VERSION}-${REVISION}"
+cd ..
+
+echo "Package is in ./dist/car-dashboard_${VERSION}-${REVISION}"
