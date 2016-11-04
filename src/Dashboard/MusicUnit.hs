11e05b300dc0f7b455e5110ffa534645d25c27d4
Henri Verroken <henriverroken@gmail.com>
2016-11-04 12:36:32 +0000

    Implement basic music playing logic in MusicUnit

--- src/Dashboard/MusicUnit.hs
+++ src/Dashboard/MusicUnit.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleContexts #-}
 -- | Unit managing the music component of the application.
 --
 -- For more functions to manage state, see 'Dashboard.MusicUnit.State'.
@@ -12,12 +14,19 @@ module Dashboard.MusicUnit (
 , updateMetadata
 , queryTrackData
 , updateTrackData
+
+  -- * Main logic
+, MusicUnitT(..)
+, startMusicUnit
 ) where
 
 import Prelude hiding (writeFile)
 
-import Control.Concurrent.STM (atomically, putTMVar, tryReadTMVar)
+import Control.Applicative ((<|>))
+import Control.Concurrent.STM (atomically, putTMVar, takeTMVar, tryReadTMVar)
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Reader (ReaderT, MonadReader, runReaderT, ask)
+import Control.Monad.State (StateT, MonadState, runStateT, get, put)
 
 import Data.ByteString.Lazy (ByteString, writeFile)
 
@@ -27,7 +36,9 @@ import Dashboard.MusicUnit.State (State(..), HasMusicState(..), emptyState,
                                   Metadata, TrackData)
 import Dashboard.Paths (getTrackDataFile)
 import Dashboard.Server.Monad (HasSettings(..))
-import Dashboard.Settings (musicCacheDir)
+import Dashboard.Settings (Settings, musicCacheDir)
+import qualified Dashboard.MusicUnit.State.Metadata as Metadata
+import qualified Dashboard.MusicUnit.State.TrackData as TrackData
 
 -- | Query the current music metadata.
 queryMetadata :: (MonadIO m, HasMusicState m) => m (Maybe Metadata)
@@ -48,3 +59,44 @@ updateTrackData x bs = do
     askSettings >>= liftIO . createDirectoryIfMissing True . musicCacheDir
     liftIO $ writeFile fp bs
     askMusicTrackData >>= liftIO . atomically . flip putTMVar x
+
+
+-- | Start the music unit
+startMusicUnit :: MonadIO m => Settings -> State -> m ()
+startMusicUnit settings state = do
+    (m, t) <- liftIO . atomically $ do
+        a <- takeTMVar (metadata state)
+        b <- takeTMVar (trackData state)
+        return (a, b)
+    let localState = LocalState m t
+    let globalState = GlobalState settings state
+    _ <- flip runReaderT globalState $ flip runStateT localState $ runMusicUnitT startMusicUnit'
+    return ()
+
+-- | State globally accessible
+data GlobalState = GlobalState { _serverSettings :: Settings
+                               , musicState :: State }
+
+-- | State local to the music unit.
+data LocalState = LocalState { stateMetadata :: Metadata
+                             , stateTrackData :: TrackData }
+
+-- | Monad transformer for the music unit.
+newtype MusicUnitT m a = MusicUnitT { runMusicUnitT :: StateT LocalState (ReaderT GlobalState m) a }
+                       deriving (Functor, Applicative, Monad, MonadIO, MonadState LocalState, MonadReader GlobalState)
+
+startMusicUnit' :: MonadIO m => MusicUnitT m ()
+startMusicUnit' = do
+    state <- musicState <$> ask
+    m <- stateMetadata <$> get
+    t <- stateTrackData <$> get
+    if Metadata.playing m && Metadata.songId m == TrackData.songId t
+        then error "not implemented: play music"
+        else error "not implemented: pause music"
+    let m' = takeTMVar (metadata state)
+    let t' = takeTMVar (trackData state)
+    update <- liftIO . atomically $ (Left <$> m') <|> (Right <$> t')
+    case update of
+        Left x -> get >>= put . (\s -> s { stateMetadata = x })
+        Right x -> get >>= put . (\s -> s { stateTrackData = x })
+    startMusicUnit'
